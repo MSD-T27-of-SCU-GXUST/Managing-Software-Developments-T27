41c3065f591b6ae77fbc3fa402995f0fe1bb8174
洩矢诹访子すわこ <118913986+Thnoab@users.noreply.github.com>
2024-10-27 02:20:28 +0800

    Delete version2.1.0 directory

--- version2.1.0/features/user_authentication/allocation_optimization/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-allocation_optimization_bp = Blueprint('allocation_optimization', __name__)
-
-from . import routes  # 导入路由文件
--- version2.1.0/features/user_authentication/database_setup/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-dorm_allocation_bp = Blueprint('dorm_allocation', __name__)
-
-from . import routes  # 导入路由文件
