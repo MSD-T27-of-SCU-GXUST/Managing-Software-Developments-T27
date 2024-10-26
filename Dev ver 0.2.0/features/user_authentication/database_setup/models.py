cec8c990fb073f19a5263d6a5394961cdfcd407d
洩矢诹访子すわこ <118913986+Thnoab@users.noreply.github.com>
2024-10-27 03:24:07 +0800

    Add files via upload

---
+++ Dev ver 0.2.0/features/user_authentication/database_setup/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Dormitory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    current_occupants = db.Column(db.Integer, default=0)
